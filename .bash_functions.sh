49224f62641770c7ab5c0e570969032d35f1f54d
Robin Winslow <robin@robinwinslow.co.uk>
2015-03-27 11:02:23 +0000

    Only use green if not root

--- .bash_functions.sh
+++ .bash_functions.sh
@@ -195,7 +195,7 @@ function home_prompt_standard ()
     prompt_settings # Get prompt colour choices
 
     # For home machine, host_colour should always be green
-    host_color=$h_green
+    [[ `whoami` != 'root' ]] && host_color=$h_green
 
     PS1="$delimiter_color[$host_color\u@\h$delimiter_color:$path_color\W\$(__git_ps1 \"$delimiter_color|$branch_color%s\")$delimiter_color] $terminus $command_color"
 }
